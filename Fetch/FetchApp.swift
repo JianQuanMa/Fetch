c356832b00e363eab98ab07ecbc66a80c2063433
Jian Ma <jianquanma@gmail.com>
2024-04-25 10:59:18 -0400

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Jian Ma on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
